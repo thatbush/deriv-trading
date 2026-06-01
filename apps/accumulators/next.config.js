97d20c332721ffba4cd1077796f1676893ed33e5
thatbush <bentley@spiritbulb.org>
2026-06-01 11:53:58 +0300

    fix asset prefix

--- apps/accumulators/next.config.js
+++ apps/accumulators/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@deriv/core'],
+  assetPrefix: 'https://deriv-trading.vercel.app'
 }
 
 module.exports = nextConfig
--- apps/digits/next.config.js
+++ apps/digits/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@deriv/core'],
+  assetPrefix: 'https://binarymatix.vercel.app'
 }
 
 module.exports = nextConfig
